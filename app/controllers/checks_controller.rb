c6b0f07742a95060ece6a56719ad17e917f18acc
Lud <lud@reinmiedl.com>
2023-06-24 14:45:26 +0200

    type all files

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,11 +1,18 @@
+# typed: strict
+
 class ChecksController < ActionController::Base
+  extend T::Sig
+
+  sig { void }
   def healthz
     return render plain: '❌ - DB connection failed. Did you setup the DB?', status: 500 unless db_alive?
 
     render plain: "✅ - #{current_version}", status: 200
   end
 
+  sig { returns(String) }
   private def current_version
+    @current_version = T.let(@current_version, T.nilable(String))
     @current_version ||= ENV.fetch('GIT_SHA', nil)
     @current_version ||= begin
       `git rev-parse HEAD`.strip
@@ -14,9 +21,11 @@ class ChecksController < ActionController::Base
     end
   end
 
+  sig { returns(T::Boolean) }
   def db_alive?
     ActiveRecord::Base.connection.active?
-  rescue StandardError
+  rescue StandardError => e
+    puts e.message
     false
   end
 end
--- app/controllers/vue_controller.rb
+++ app/controllers/vue_controller.rb
@@ -1,6 +1,11 @@
+# typed: strict
+
 class VueController < ActionController::Base
+  extend T::Sig
+
   layout nil
 
+  sig { void }
   def index
     respond_to do |format|
       format.html
